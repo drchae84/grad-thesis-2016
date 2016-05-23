905f0898c1d441926ea62377b9eb78a53ef64de8
drchae84 <chae.office@gmail.com>
2016-05-23 17:41:36 -0400

    updates
    added the intro, and directory animation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,90 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(document).ready(function() {
+// INTRO ANIMATION
+$('#intro').click(function () {
 
-	// Prevent default anchor click behavior
-	event.preventDefault();
+    var windowHeight = $(window).height();
+    var lineHeight = $('#line').height();
+    var desiredBottom = 0;
+    var desiredTop = -1000;
+    var newPosition = windowHeight - (lineHeight + desiredBottom);
 
-	// Store hash
-	var hash = this.hash;
 
-	// Using jQuery's animate() method to add smooth page scroll
-	// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-	$('html, body').animate({
-	  scrollTop: $(hash).offset().top
-	}, 800, function(){
+    $('#line').animate({top:-5000},3000,function () {
+        $('#line').css({
+            top: -5000,
+        });
+    });
+    $('#line2').animate({top:5000},3000,function () {
+        $('#line2').css({
+            bottom: 5000,
+            top: 'auto'
+        });
+    });
 
-	  // Add hash (#) to URL when done scrolling (default click behavior)
-	  window.location.hash = hash;
-	});
+    $('#intro').animate({opacity:0},4000, function(){
+        $('#intro').css({
+            opacity: 0,
+        });
+    });
+console.log('intro is loaded');
+});
+
+// ANCHOR LINK SMOOTH SCROLLING //
+
+    $("a").on('click', function(event) {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+    });
+
+
+    function sticky_relocate() {
+        var window_top = $(window).scrollTop();
+        var div_top = $('#sticky-anchor').offset().top;
+        if (window_top > div_top) {
+            $('#sticky').addClass('stick');
+        } else {
+            $('#sticky').removeClass('stick');
+        }
+    }
+    console.log('hello sticky');
+
+    $(function() {
+        $(window).scroll(sticky_relocate);
+        sticky_relocate();
+    });
+
+    $ ("#sticky a").on('click', function(event) {
+      // event.preventDefault();
+
+/*makes list visible*/
+      $( "#list" ).removeClass( "invisible" );
+    });
+    console.log('hello student list');
+
+
+// THUMBNAIL DISPLAY
+$('#list p').hover(function() {
+    var idx = $('#list p').index(this);
+
+    $('.thumb').eq(idx).css('display', 'block');
+  }, function() {
+    var idx = $('#list p').index(this);
+    $('.thumb').eq(idx).css('display', 'none');
   });
+
+    console.log('student list');
 });
